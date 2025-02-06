737e55a223b9410cfeb4f3f91ad23ee3468c0e77
Alexis Roman <alexis.roman@unl.edu.ec>
2025-02-06 09:49:55 -0500

    add register with form

--- controller/users_con.py
+++ controller/users_con.py
@@ -5,29 +5,29 @@ from config import mysql
 
 user_controller = Blueprint('user_controller', __name__)
 
-@user_controller.route('/registro', methods=['POST'])
+@user_controller.route('/bd/registro/save', methods=['POST'])
 def regis_user():
-    nombres = request.form['nombres']
-    apellidos = request.form['apellidos']
-    fecha_nacimiento = request.form['fecha_nacimiento']
-    email = request.form['email']
-    celular = request.form['celular']
-
-    usuario = request.form['usuario']
-    contrasena = request.form['contrasena']
+    data = request.json
+    print('OBTENGO EL JSON')
+    print(data)
     
-    cur = mysql.connection.cursor() #Comenzar el cursor sql
-    #Insertar
-    cur.execute(f"INSERT INTO Persona (nombres, apellidos, fecha_nacimiento, email, celular) VALUES ('{nombres}', '{apellidos}', '{fecha_nacimiento}', '{email}', '{celular}')")
+    try:
+        cur = mysql.connection.cursor() #Comenzar el cursor sql
+        #Insertar
+        cur.execute(f"INSERT INTO Persona (nombres, apellidos, fecha_nacimiento, email, celular) VALUES ('{data['nombres']}', '{data['apellidos']}', '{data['fecha_nacimiento']}', '{data['email']}', '{data['celular']}')")
 
-    iden_persona = cur.lastrowid #Obtener el id de persona
+        iden_persona = cur.lastrowid #Obtener el id de persona
 
-    cur.execute(f"INSERT INTO Cuenta (identificacion, usuario, contrasena) VALUES ('{iden_persona}', '{usuario}', '{contrasena}')")
-    #Guardar
-    mysql.connection.commit()
-    cur.close()
-    
-    return jsonify({'message': 'Usuario registrado correctamente'}), 200
+        cur.execute(f"INSERT INTO Cuenta (identificacion, usuario, contrasena) VALUES ('{iden_persona}', '{data['usuario']}', '{data['contrasena']}')")
+        #Guardar
+        mysql.connection.commit()
+        cur.close()
+        
+        return jsonify({'msg': 'Ok', 'data': 'Usuario registrado correctamente'}), 200
+    except Exception as e:
+        print('llego al error')
+        print(e)
+        return jsonify({'msg': 'Error', 'data': str(e)}), 400
 
 @user_controller.route('/login', methods=['POST'])
 def login_user():
